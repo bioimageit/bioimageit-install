802d92f776d1f3b0a26625da4e24b95dacfa2129
PRIGENT Sylvain <sylvain.prigent@inria.fr>
2021-04-26 15:59:30 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -5,46 +5,25 @@ cd $BASEDIR
 
 # check inputs
 if [ -z "$1" ]; then
-    read -p "Conda directory: "  in_conda_dir
-else
-    in_conda_dir="$1"
-fi 
-check_conda
-if [ -z "$2" ]; then
     read -p "Destination directory: "  in_destination_dir
 else
-    in_destination_dir="$2"
+    in_destination_dir="$1"
 fi    
 check_destination $in_destination_dir
-if [ -z "$3" ]; then
+if [ -z "$2" ]; then
     read -p "User name: "  in_username
 else
-    in_username="$3"
+    in_username="$2"
 fi 
-if [ -z "$4" ]; then
+if [ -z "$3" ]; then
     read -p "Backend (Local, Docker, Singularity, Allgo)"  in_backend
 else
-    in_backend="$4"
+    in_backend="$3"
 fi 
-check_backend $in_backend
-create_conda_env $in_conda_dir
-
 
 echo "destination dir: $in_destination_dir"
 echo "username: $in_username"
 echo "backend: $in_backend"
-
-
-
-check_conda(){
-    if ! command -v conda -h &> /dev/null
-    then
-        echo "COMMAND conda could not be found. You need to install conda before installing BioImageIT"
-        exit 1
-    else
-        echo "Conda found !"    
-    fi
-}
 
 check_destination(){
     if [ -d "$1" ]; then
@@ -65,15 +44,42 @@ check_backend(){
     fi
 }
 
-create_conda_env(){
-    compt = 0
-    while [ -d "$1/bioimageit$compt" ]; do
-        $compt=$compt+1
-    done
-    conda create --name "$1/bioimageit$compt"
-    conda activate "bioimageit$compt"
-    conda install -c bioimageit biomiageit_core
-    conda install -c bioimageit bioimageit_gui
-    python bioimagepy/config.py "${2}" # this command should be not good
-    python bioimageapp/config.py # this command should be not good
+intall_bioimageit(){
+    in_destination_dir=$1
+    in_username=$2
+    in_backend=$3
+
+    cd $in_destination_dir
+    python -m venv .bioimageit-env
+    source .bioimageit-env/bin/activate
+
+    git clone https://github.com/bioimageit/bioimageit_core.git
+    git clone https://github.com/bioimageit/bioimageit_gui.git
+    git clone https://github.com/bioimageit/bioimageit_viewer.git
+    git clone https://github.com/bioimageit/bioimageit-toolboxes.git
+    git clone https://github.com/bioimageit/bioimageit-package.git
+
+    # create toolboxes database
+    mkdir toolboxes
+    cp -r bioimageit-toolboxes/thumbs toolboxes
+    cp bioimageit-toolboxes/toolboxes.json toolboxes/toolboxes.json
+    cp bioimageit-toolboxes/tools.json toolboxes/tools.json
+    cp bioimageit-toolboxes/formats.json ./formats.json
+    rm -rf ./bioimageit-toolboxes
+
+    # create shortcuts
+    cp bioimageit-package/scripts/data_management.sh ./data_management.sh
+    cp bioimageit-package/scripts/data_processing.sh ./data_processing.sh
+    cp bioimageit-package/scripts/jupyter.sh ./jupyter.sh
+
+
+    pip install ./bioimageit_core
+    pip install ./bioimageit_gui
+    pip install ./bioimageit_viewer
+    python bioimageit_core/config.py "${in_username}" "${in_backend}"
+    python bioimageit_gui/config.py 
 }
+
+check_destination $in_destination_dir
+check_backend $in_backend
+intall_bioimageit $in_destination_dir $in_username $in_backend
