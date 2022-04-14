d6ac2a19ff37b501deae5955b9614c48f59a2d59
leO9764 <leomaury97@gmail.com>
2022-04-14 12:04:03 +0200

    minor changes during uninstallation

--- windows/uninstall_bioimageit.bat
+++ windows/uninstall_bioimageit.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFDV7YkSjGEaTKIk45+vu4u+Jtl4hduswcYLa3bGnFN8a5FbwdJoh125Tl8UzChJUewGXbAY7oGNDsWGMI4qVvRz1S0SF2lw5FWF7kHDsmSUybsB8m88P7CK380TynqIV2GyxW7ELdQ==
-::YB416Ek+Zm8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 rem ARE YOU SURE YOU WANT TO UNINSTALL BIOIMAGEIT ? If YES, press ENTER
 
 pause
@@ -40,7 +7,7 @@ rmdir /s /q "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Pr
 del "C:\Users\%USERNAME%\Desktop\BioimageIT.lnk"
 
 
-rem DONE !
+rem Shortcuts removed
 
 
 rem UNINSTALL CONDA ? If YES, press ENTER. If NO, just  close the window
@@ -49,11 +16,14 @@ pause
 
 start /D "C:\Users\%USERNAME%\BioImageIT\Miniconda3" Uninstall-Miniconda3.exe
 
+rem Miniconda uninstalled
+
 pause
 
 if not exist "C:\Users\%USERNAME%\BioImageIT\Miniconda3\Uninstall-Miniconda3.exe" rmdir /s /q "C:\Users\%USERNAME%\BioImageIT"
 
+rem BioImageIT removed
 
-rem UNINSTALLATION FINISHED
+rem UNINSTALLATION FINISHED, do not hesitate do install it again ;)
 
 pause
