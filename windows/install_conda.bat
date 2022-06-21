482011bb41117abac6a73fa86c96e741881947d0
Léo Maury <42140279+leomaury@users.noreply.github.com>
2022-06-21 10:43:10 +0200

    Update install_conda.bat

--- windows/install_conda.bat
+++ windows/install_conda.bat
@@ -34,8 +34,8 @@ set conda_path="%bioimageit_dir%\Miniconda3\condabin\conda.bat"
 call %conda_path% upgrade conda -y
 call %conda_path% config --add channels conda-forge
 call %conda_path% config --add channels bioimageit
-call %conda_path% install -y -c conda-forge micromamba
-micromamba create -y -n bioimageit python=3.9
+call %conda_path% install -y -c conda-forge mamba
+mamba create -y -n bioimageit python=3.9
 
 
 rem call install.bat
@@ -45,9 +45,9 @@ set python_path="%bioimageit_dir%\Miniconda3\envs\bioimageit\python.exe"
 
 call %miniconda_path%\Scripts\activate.bat bioimageit
 
-micromamba install -y git 
-micromamba install -y -c ome omero-py
-micromamba install -y -c conda-forge gitpython
+mamba install -y git 
+mamba install -y -c ome omero-py
+mamba install -y -c conda-forge gitpython
 
 
 rem call install_aftergit.bat
