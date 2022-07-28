0312c60ed84ba1306cd442ee257a9a92c957dd56
Léo Maury <42140279+leomaury@users.noreply.github.com>
2022-07-28 15:56:06 +0200

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -11,31 +11,25 @@ python_path="$installdir/miniconda3/bin/python"
 pip_path="$installdir/miniconda3/bin/pip"
 
 
-
-# create the install directory
-cd $userdir
-mkdir -p "$installdir"
-cd "$installdir"
-
 # create log file
 exec >$installdir/install.log 2>&1
 
 ########## install_main.sh ##########
 
 wget https://raw.githubusercontent.com/bioimageit/bioimageit-install/main/linux/install_main.sh && chmod +x install_main.sh && sh install_main.sh
-
+echo "25 %"
 
 
 ########## install_fiji.sh ##########
 
 wget https://raw.githubusercontent.com/bioimageit/bioimageit-install/main/linux/install_fiji.sh && chmod +x install_fiji.sh && sh install_fiji.sh
-
+echo "50 %"
 
 
 ########## install_shortcuts.sh ##########
 
 wget https://raw.githubusercontent.com/bioimageit/bioimageit-install/main/linux/install_shortcuts.sh && chmod +x install_shortcuts.sh && sh install_shortcuts.sh
-
+echo "75 %"
 
 ########## Remove sh files ##########
 cd $userdir
@@ -43,3 +37,6 @@ cd $userdir
 rm install_main.sh
 rm install_fiji.sh
 rm install_shortcuts.sh
+
+echo "100 %"
+echo "BioImageIT is installed."
