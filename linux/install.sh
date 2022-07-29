7c092b61ce698d5f7dc8e77a1e0c5a9ea62e514e
Léo Maury <42140279+leomaury@users.noreply.github.com>
2022-07-29 10:25:17 +0200

    --branch v0.1.2

--- linux/install.sh
+++ linux/install.sh
@@ -13,19 +13,19 @@ pip_path="$installdir/miniconda3/bin/pip"
 
 ########## install_main.sh ##########
 
-wget https://raw.githubusercontent.com/bioimageit/bioimageit-install/main/linux/install_main.sh && chmod +x install_main.sh && sh install_main.sh
+wget https://raw.githubusercontent.com/bioimageit/bioimageit-install/v0.1.2/linux/install_main.sh && chmod +x install_main.sh && sh install_main.sh
 echo "25 %"
 
 
 ########## install_fiji.sh ##########
 
-wget https://raw.githubusercontent.com/bioimageit/bioimageit-install/main/linux/install_fiji.sh && chmod +x install_fiji.sh && sh install_fiji.sh
+wget https://raw.githubusercontent.com/bioimageit/bioimageit-install/v0.1.2/linux/install_fiji.sh && chmod +x install_fiji.sh && sh install_fiji.sh
 echo "50 %"
 
 
 ########## install_shortcuts.sh ##########
 
-wget https://raw.githubusercontent.com/bioimageit/bioimageit-install/main/linux/install_shortcuts.sh && chmod +x install_shortcuts.sh && sh install_shortcuts.sh
+wget https://raw.githubusercontent.com/bioimageit/bioimageit-install/v0.1.2/linux/install_shortcuts.sh && chmod +x install_shortcuts.sh && sh install_shortcuts.sh
 echo "75 %"
 
 ########## Remove sh files ##########
