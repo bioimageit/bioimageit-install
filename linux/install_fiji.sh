0139c316ea8d8a17150f71964015f9c891473258
KevinFournier <dev@kfournier.eu>
2023-08-29 10:33:07 +0200

    Add the overwriting files option when unziping Fiji to avoid being asked the question in a sub terminal

--- linux/install_fiji.sh
+++ linux/install_fiji.sh
@@ -8,7 +8,7 @@ exec >$installdir/install.log 2>&1
 cd $installdir
 
 wget https://downloads.imagej.net/fiji/$fiji_version/fiji-linux64.zip
-unzip fiji-linux64.zip
+unzip -o fiji-linux64.zip
 rm fiji-linux64.zip
 
 cp -a ./toolboxes/tools/fiji_utils/. ./Fiji.app/macros
