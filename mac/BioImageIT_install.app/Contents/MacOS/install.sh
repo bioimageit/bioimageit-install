c33b257e5e3e6f8a97443ec52fc3400432ad0d3f
KevinFournier <dev@kfournier.eu>
2023-08-31 11:12:58 +0200

    clean mac install script. use config file.

--- mac/BioImageIT_install.app/Contents/MacOS/install.sh
+++ mac/BioImageIT_install.app/Contents/MacOS/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+. ./install.conf
 
 install_miniconda(){
     installdir=$1
@@ -9,34 +10,61 @@ install_miniconda(){
     bash "$installdir/miniconda3/miniconda.sh" -b -u -p "$installdir/miniconda3/"
     rm -rf $installdir/miniconda3/miniconda.sh
 
-    chmod +x "$installdir/miniconda3/etc/profile.d/conda.sh"
-    . "$installdir/miniconda3/etc/profile.d/conda.sh" && conda upgrade conda -y
-    . "$installdir/miniconda3/etc/profile.d/conda.sh" && conda config --add channels conda-forge
-    . "$installdir/miniconda3/etc/profile.d/conda.sh" && conda config --add channels bioimageit
+    chmod +x "$conda_sh"
+    . "$conda_sh" && conda upgrade conda -y
+    . "$conda_sh" && conda config --add channels conda-forge
+    . "$conda_sh" && conda config --add channels bioimageit
 }   
 
 setup_bioimageit(){
-    in_destination_dir=$1
-    python_path=$2
-    pip_path=$3
-    in_username=$4
-    in_backend=$5
+    in_username=$1
+    in_backend=$2
 
-    cd $in_destination_dir 
+    framework_repo=https://github.com/bioimageit/bioimageit_framework.git
+    [[ ! -z "$biit_framework_tag" ]] && framework_repo+=" --branch ${biit_framework_tag}"
+    
+    core_repo=https://github.com/bioimageit/bioimageit_core.git
+    [[ ! -z "$biit_core_tag" ]] && core_repo+=" --branch ${biit_core_tag}"
+    
+    gui_repo=https://github.com/bioimageit/bioimageit_gui.git
+    [[ ! -z "$biit_gui_tag" ]] && gui_repo+=" --branch ${bitt_gui_tag}"
+    
+    formats_repo=https://github.com/bioimageit/bioimageit_formats.git
+    [[ ! -z "$biit_formats_tag" ]] && formats_repo+=" --branch ${biit_formats_tag}"
+    
+    viewer_repo=https://github.com/bioimageit/bioimageit_viewer.git
+    [[ ! -z "$biit_viewer_tag" ]] && viewer_repo+=" --branch ${biit_viewer_tag}"
+    
+    toolboxes_repo=https://github.com/bioimageit/bioimageit-toolboxes.git
+    [[ ! -z "$biit_toolboxes_tag" ]] && toolboxes_repo+=" --branch ${biit_toolboxes_tag}"
+    
+    tools_repo=https://github.com/bioimageit/bioimageit-tools.git
+    [[ ! -z "$biit_tools_tag" ]] && tools_repo+=" --branch ${biit_tools_tag}"
+    
+    package_repo=https://github.com/bioimageit/bioimageit-package.git
+    [[ ! -z "$biit_package_tag" ]] && package_repo+=" --branch ${biit_package_tag}"
+    
+    notebooks_repo=https://github.com/bioimageit/bioimageit-notebooks.git
+    [[ ! -z "$biit_notebooks_tag" ]] && notebooks_repo+=" --branch ${biit_notebooks_tag}"
+    
+    omero_repo=https://github.com/bioimageit/bioimageit-omero.git
+    [[ ! -z "$biit_omero_tag" ]] && omero_repo+=" --branch ${biit_omero_tag}"
+
 
     # main app
-    git clone https://github.com/bioimageit/bioimageit_framework.git --branch v0.1.3
-    git clone https://github.com/bioimageit/bioimageit_core.git --branch v0.1.3
-    git clone https://github.com/bioimageit/bioimageit_gui.git --branch v0.1.3
-    git clone https://github.com/bioimageit/bioimageit_formats.git --branch v0.1.3
-    git clone https://github.com/bioimageit/bioimageit_viewer.git --branch v0.1.3
-    git clone https://github.com/bioimageit/bioimageit-toolboxes.git
-    git clone https://github.com/bioimageit/bioimageit-tools.git
-    git clone https://github.com/bioimageit/bioimageit-package.git
-    git clone https://github.com/bioimageit/bioimageit-notebooks.git
-
+    cd $installdir
+    . "$conda_sh" && conda activate bioimageit && git clone $framework_repo
+    . "$conda_sh" && conda activate bioimageit && git clone $core_repo
+    . "$conda_sh" && conda activate bioimageit && git clone $gui_repo
+    . "$conda_sh" && conda activate bioimageit && git clone $formats_repo
+    . "$conda_sh" && conda activate bioimageit && git clone $viewer_repo
+    . "$conda_sh" && conda activate bioimageit && git clone $toolboxes_repo
+    . "$conda_sh" && conda activate bioimageit && git clone $tools_repo
+    . "$conda_sh" && conda activate bioimageit && git clone $package_repo
+    . "$conda_sh" && conda activate bioimageit && git clone $notebooks_repo
+    
     # plugins
-    git clone https://github.com/bioimageit/bioimageit-omero.git --branch v0.1.3
+    . "$conda_sh" && conda activate bioimageit && git clone $omero_repo
 
     # create toolboxes database
     mkdir toolboxes
@@ -49,6 +77,7 @@ setup_bioimageit(){
     # create shortcuts
     cp bioimageit-package/mac/BioImageIT.sh BioImageIT.sh
     cp bioimageit-package/mac/jupyter.sh jupyter.sh
+
     cp -r bioimageit-package/mac/BioImageIT.app/ /Applications/BioImageIT.app/
     cp -r "bioimageit-package/mac/BioImageIT Workspace.app/" "/Applications/BioImageIT Workspace.app/"
     cp -r "bioimageit-package/mac/BioImageIT Jupyter.app/" "/Applications/BioImageIT Jupyter.app/"
@@ -61,30 +90,22 @@ setup_bioimageit(){
     mkdir workspace/logs
 
     # install and config packages
-    . "$installdir/miniconda3/etc/profile.d/conda.sh" && conda activate bioimageit && pip install ./bioimageit_formats
-    . "$installdir/miniconda3/etc/profile.d/conda.sh" && conda activate bioimageit && pip  install ./bioimageit_framework
-    . "$installdir/miniconda3/etc/profile.d/conda.sh" && conda activate bioimageit && pip  install ./bioimageit_core
-    . "$installdir/miniconda3/etc/profile.d/conda.sh" && conda activate bioimageit && pip  install ./bioimageit_gui
-    . "$installdir/miniconda3/etc/profile.d/conda.sh" && conda activate bioimageit && pip  install ./bioimageit_viewer
-    . "$installdir/miniconda3/etc/profile.d/conda.sh" && conda activate bioimageit && pip  install ./bioimageit-omero
-    . "$installdir/miniconda3/etc/profile.d/conda.sh" && conda activate bioimageit && pip  install jupyter
-    . "$installdir/miniconda3/etc/profile.d/conda.sh" && conda activate bioimageit && python  bioimageit_core/config.py "${in_username}" "${in_backend}"
-    . "$installdir/miniconda3/etc/profile.d/conda.sh" && conda activate bioimageit && python bioimageit_gui/config.py 
+    . "$conda_sh" && conda activate bioimageit && pip install ./bioimageit_formats
+    . "$conda_sh" && conda activate bioimageit && pip install ./bioimageit_framework
+    . "$conda_sh" && conda activate bioimageit && pip install ./bioimageit_core
+    . "$conda_sh" && conda activate bioimageit && pip install ./bioimageit_gui
+    . "$conda_sh" && conda activate bioimageit && pip install ./bioimageit_viewer
+    . "$conda_sh" && conda activate bioimageit && pip install ./bioimageit-omero
+    . "$conda_sh" && conda activate bioimageit && pip install jupyter
+    . "$conda_sh" && conda activate bioimageit && python bioimageit_core/config.py "${in_username}" "${in_backend}"
+    . "$conda_sh" && conda activate bioimageit && python bioimageit_gui/config.py 
 }
 
 ######################## MAIN #######################
-backend="CONDA"
 
-# setup usefull path
-userdir="/Users/$USER"
-
-installdir="$userdir/BioImageIT"
-conda_bin="$installdir/miniconda3/condabin/conda"
-conda_sh="$installdir/miniconda3/etc/profile.d/conda.sh"
-python_path="$installdir/miniconda3/envs/bioimageit/bin/python"
-pip_path="$installdir/miniconda3/envs/bioimageit/bin/pip"
 
 # create the install directory
+userdir="/Users/$USER"
 cd $userdir
 mkdir -p "$installdir"
 cd "$installdir"
@@ -93,23 +114,25 @@ cd "$installdir"
 exec >$installdir/installation.log 2>&1
 
 # install Local Miniconda
-install_miniconda $installdir
+install_miniconda
 
 # create bioimageit env
-. "$installdir/miniconda3/etc/profile.d/conda.sh" && conda create -y --name bioimageit python=3.9
-. "$installdir/miniconda3/etc/profile.d/conda.sh" && conda activate bioimageit && conda install -y git
-. "$installdir/miniconda3/etc/profile.d/conda.sh" && conda activate bioimageit && conda install -y -c conda-forge gitpython
-. "$installdir/miniconda3/etc/profile.d/conda.sh" && conda activate bioimageit && conda install -y -c ome omero-py
+. "$conda_sh" && conda create -y --name bioimageit python=3.9
+
+# install git
+. "$conda_sh" && conda activate bioimageit && conda install -y git
+. "$conda_sh" && conda activate bioimageit && conda install -y -c conda-forge gitpython
+. "$conda_sh" && conda activate bioimageit && conda install -y -c ome omero-py
 
 # clone and setup BioImageIT
-setup_bioimageit $installdir $python_path $pip_path $USER "CONDA" 
+setup_bioimageit $USER $backend
 
 
 ###################### FIJI ###########################
 cd $installdir
 
-curl https://downloads.imagej.net/fiji/latest/fiji-macosx.zip -o Fiji.zip
-unzip Fiji.zip
+curl https://downloads.imagej.net/$fiji_version/latest/fiji-macosx.zip -o Fiji.zip
+unzip -o Fiji.zip
 rm Fiji.zip
 
 cp -a ./toolboxes/tools/fiji_utils/. ./Fiji.app/macros
